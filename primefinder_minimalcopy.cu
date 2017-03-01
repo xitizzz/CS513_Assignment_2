33c947c9c59736d0dd380d9d175f030ce46869d7
Michael Sherman <msherman@winlab.rutgers.edu>
2017-02-28 23:28:21 -0500

    using host shared memory to communicate next loop id back to host

--- primefinder_minimalcopy.cu
+++ primefinder_minimalcopy.cu
@@ -6,7 +6,7 @@
    slow. We must use multiplication instead of repeated addition to limit
    error accumulation, but floats should be workable. */
 __global__
-void multiples(uint32_t step, uint32_t *array, uint32_t n){
+void multiples(uint32_t step, uint32_t *array, uint32_t n, uint32_t *next_prime){
     /*this function should compute all multiples of step, and mark 
     indices of prime_array at those points. This function should use
     threadID to divide the problem set */
@@ -42,23 +42,40 @@ void multiples(uint32_t step, uint32_t *array, uint32_t n){
         for(int i = start_mult; i<=end_mult; i++){
             array[step * i] = 1;
         }
+        __syncthreads(); //barrier until all threads done
+        int j = step+1;
+        for(; j<n; j++){
+            if(array[j]==0){
+                *next_prime = j;//write host memory
+                break;
+            }
+        }
+        if(j == n){
+            *next_prime = 0;
+        }
 
 
     }
 }
 
-
-
 int main(){
     uint32_t n = 1<<10; //find all primes upto and including this number
     uint32_t *prime_array = (uint32_t *)calloc(n , sizeof(uint32_t)); //allocate and zero
+    uint32_t *next_prime = (uint32_t *)malloc(sizeof(uint32_t)); //allocate index for signalling
+    cudaMallocHost(&next_prime, sizeof(uint32_t));
+    *next_prime = 2; //initialize
+
     uint32_t *d_array;
     cudaMalloc(&d_array, n * sizeof(uint32_t));
     cudaMemcpy(d_array, prime_array, n * sizeof(uint32_t), cudaMemcpyHostToDevice);
 
-    for(int loop = 2; loop <= sqrt(n); loop++){ //TODO careful of sqrt here, check for primes only
-        multiples<<<1,32>>>(loop,d_array,n);
+
+    for(int loop = 2; loop <= sqrt(n); loop = *next_prime){ //TODO careful of sqrt here, check for primes only
+//    while(*next_prime !=0){
+        printf("starting loop for multiples of %d\n", loop);
+        multiples<<<1,1>>>(loop,d_array,n,next_prime);
         cudaDeviceSynchronize();
+        printf("next prime is %d\n", *next_prime);
     }
 
     cudaMemcpy(prime_array, d_array, n * sizeof(uint32_t), cudaMemcpyDeviceToHost);
